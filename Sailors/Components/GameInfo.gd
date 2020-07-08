f81284b8f9f7824f6abcb5407dc7371eb5715703
frkmnt <pootispow74@gmail.com>
2020-07-08 02:00:14 +0100

    Added new cards
    About 30

--- Sailors/Components/GameInfo.gd
+++ Sailors/Components/GameInfo.gd
@@ -16,11 +16,25 @@ func initialize():
 
 func initialize_deck():
 	# choose cards 
-	a_deck.append([ [0, 1], [1, 1] ]) 
+	a_deck.append \
+	([ 
+		[0, 1], [1, 1], [2, 1], [3, 1], [4, 1]
+	]) 
+	
 	# do cards 
-	a_deck.append([ [0, 1], [1, 1] ]) 
+	a_deck.append \
+	([ 
+		[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], \
+		[6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1],  
+		[12, 1], [13, 1], [14, 1], [15, 1], [16, 1], [17, 1],
+		[18, 1], [19, 1], [20, 1], [21, 1]
+	]) 
+	
 	# keep cards 
-	a_deck.append([ [0, 1] ]) 
+	a_deck.append \
+	([ 
+		[0, 1] 
+	]) 
 
 
 func initialize_players(player_names):
